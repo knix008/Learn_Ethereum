10aa3e37d075db6ae6f8bb3abe838d0b7f611d2c
Suho Kwon <suho.kwon@gmail.com>
2023-11-02 17:24:13 +0900

    Add an example solidity code.

---
+++ chapter6/Person.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract MyStruct {
+    struct person {
+        uint age;
+        string firstName;
+        string lastName;
+        string emailAddress;
+        string cellPhone;
+    }
+} 
