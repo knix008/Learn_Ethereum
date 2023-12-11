28844c335eba42374ab5040a66382bb6834b0d6f
Suho Kwon <suho.kwon@gmail.com>
2023-12-11 15:50:35 +0900

    Change truffle configuration.

--- chapter08/MyERC20Tokens/contracts/Migrations.sol
+++ chapter08/MyERC20Tokens/contracts/Migrations.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: No License
+pragma solidity ^0.8.9;
 
 contract Migrations {
   address public owner;
@@ -8,7 +9,7 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  constructor() public {
+  constructor() {
     owner = msg.sender;
   }
 
