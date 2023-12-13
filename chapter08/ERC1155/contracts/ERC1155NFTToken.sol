db6592f2bdf6e21dadd8a9e3f14f59b68c777295
Suho Kwon <suho.kwon@gmail.com>
2023-12-13 15:50:37 +0900

    Add truffle and ganache working example.

--- chapter08/ERC1155/contracts/ERC1155NFTToken.sol
+++ chapter08/ERC1155/contracts/ERC1155NFTToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts@4.9.3/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract ERC1155NFTToken is ERC1155 {
     uint256 public constant MomentOfSilence = 0;
--- chapter08/ERC1155/migrations/2_ERC1155NFTToken_migration.js
+++ chapter08/ERC1155/migrations/2_ERC1155NFTToken_migration.js
@@ -1,5 +1,5 @@
 var ERC1155NFTToken = artifacts.require("./ERC1155NFTToken");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC1155NFTToken, name, symbol);
+  deployer.deploy(ERC1155NFTToken);
 };
