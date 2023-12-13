a1804ec40b0a39a4a291e53e129a4a3e37fbdb48
knix008 <suho.kwon@gmail.com>
2023-12-13 13:12:55 +0900

    Change solidity files.

--- chapter08/SimpleStoreInterface.sol
+++ chapter08/SimpleStoreInterface.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4.0;
+pragma solidity ^0.8.9;
+
 /**
  *@notice smart contract for SimpleStoreInterface
  * @ Learn Ethereum 2nd Edition
  * @author brian wu
- */ 
-contract SimpleStoreInterface {
-   function getValue() external view returns (uint256);
+ */
+interface SimpleStoreInterface {
+    function getValue() external view returns (uint256);
 }
--- chapter08/SimpleStoreReader.sol
+++ chapter08/SimpleStoreReader.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4.0;
+pragma solidity ^0.8.9;
 /**
  *@notice smart contract for SimpleStoreReader
  * @ Learn Ethereum 2nd Edition
  * @author brian wu
- */ 
-import “./SimpleStoreInterface.sol”;
+ */
+import "./SimpleStoreInterface.sol";
+
 contract SimpleStoreReader {
-  function getValue(address simpleStore) 
-    external view returns (uint256) {
-    return SimpleStoreInterface(simpleStore).getValue();
-  }
+    function getValue(address simpleStore) external view returns (uint256) {
+        return SimpleStoreInterface(simpleStore).getValue();
+    }
 }
