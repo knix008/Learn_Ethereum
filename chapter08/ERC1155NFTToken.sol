6ffa668529ec634c958c7f8e8c291fde09b9a58c
Suho Kwon <suho.kwon@gmail.com>
2023-12-13 14:06:57 +0900

    Add ERC-1155.sol.

--- chapter08/ERC1155NFTToken.sol
+++ chapter08/ERC1155NFTToken.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
  *@notice smart contract for The DigitalArt ERC721 Token
  * @ Learn Ethereum 2nd Edition
  * @author brian wu
- */ 
+ */
 
 ///import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract ERC1155NFTToken  is ERC1155 {
+contract ERC1155NFTToken is ERC1155 {
     uint256 public constant MomentOfSilence = 0;
     uint256 public constant Finchwing = 1;
     uint256 public constant GirlAndBird = 2;
@@ -18,7 +18,11 @@ contract ERC1155NFTToken  is ERC1155 {
     uint256 public constant Nighthill = 5;
     uint256 public constant Storm = 6;
 
-    constructor() ERC1155("https://ipfs.io/ipfs/bafybeifcatfkx7jehcieim4ujuf6rin7jqcqjkqwdvtra3stpndgmznydm/{id}.json") {
+    constructor()
+        ERC1155(
+            "https://ipfs.io/ipfs/bafybeifcatfkx7jehcieim4ujuf6rin7jqcqjkqwdvtra3stpndgmznydm/{id}.json"
+        )
+    {
         _mint(msg.sender, MomentOfSilence, 10, "");
         _mint(msg.sender, Finchwing, 10, "");
         _mint(msg.sender, GirlAndBird, 10, "");
