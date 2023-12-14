2c3694ec5833e6ab4e17aaf837c26c99e542f7f2
Suho Kwon <34533493+knix008@users.noreply.github.com>
2023-12-14 17:28:22 +0900

    Create Storage.sol

---
+++ chapter09/Storage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: No License
+pragma solidity ^0.8.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Storage {
+
+    uint256 private number;
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
