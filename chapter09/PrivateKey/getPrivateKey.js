2bcf4b50401986cdf1e60fc439fc6c734b3a838e
Suho Kwon <suho.kwon@gmail.com>
2023-12-14 16:18:08 +0900

    Add private key decode.

--- chapter09/PrivateKey/getPrivateKey.js
+++ chapter09/PrivateKey/getPrivateKey.js
@@ -4,7 +4,7 @@ var address = "0xbbb4a9750dbe0d03207c9b0d1b1778f3b65d44cc";
 var dir = "/home/shkwon/private-chain"; //keystore가 있는 data 디렉토리 경로
 
 var keyObject = keythereum.importFromFile(address, dir);
-var pk = keythereum.recover('<You password for the account>', keyObject);
+var pk = keythereum.recover('<Put your password here!!!>', keyObject);
 
 var privateKey = pk.toString('hex');
 console.log(privateKey);
