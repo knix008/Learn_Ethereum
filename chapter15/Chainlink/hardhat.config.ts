5bc6141c2050f83bca838e951de2438d7adf295a
Suho Kwon <suho.kwon@gmail.com>
2024-01-29 15:52:21 +0900

    Add Chainlink VRF example.

---
+++ chapter15/Chainlink/hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const ALCHEMY_API_KEY = "yEweLj8PJaObGdzgif_Y7iP1Rg7L2lfy";
+const PRIVATE_KEY = "4545954a61d55ea366aff98249030c237f0ac7e2c6c48bb4e8d0f1579f365ecb";
+const config: HardhatUserConfig = {
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/yEweLj8PJaObGdzgif_Y7iP1Rg7L2lfy`,
+      accounts: [PRIVATE_KEY]
+    },
+  },
+  solidity: "0.8.17",
+};
+
+export default config;
---
+++ chapter15/Chainlink/scripts/deploy.ts
@@ -0,0 +1,33 @@
+import { ethers } from "hardhat";
+
+/**
+ * run below command to deploy contract
+ * npx hardhat run scripts/deploy.ts --network sepolia
+ */
+async function main() {
+
+  const LINK_TOKEN = "0x779877a7b0d9e8603169ddbd7836e478b4624789";
+  const VRF_COORDINATOR = "0x8103b0a8a00be2ddc778e6e7eaa21791cd364625";
+  const KEY_HASH = "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c";
+  const diceRollGame = await ethers.getContractFactory("DiceRollGame");
+  // deploy the contract
+  const deployedDiceRollGameContract = await diceRollGame.deploy(
+    VRF_COORDINATOR,
+    LINK_TOKEN,
+    KEY_HASH
+  );
+
+  await deployedDiceRollGameContract.deployed();
+  // print the address of the deployed contract
+  console.log("Dice Roll Game Contract Address:", deployedDiceRollGameContract.address);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
